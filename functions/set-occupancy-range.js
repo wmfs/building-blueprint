4b2a28e66e9bc1dbdc1742cef6a75f1363bfa4f3
wmfsConorF <conor.flanagan@wmfs.net>
2022-05-03 16:36:50 +0100

    style: removed console logs from set-occupancy-range

--- functions/set-occupancy-range.js
+++ functions/set-occupancy-range.js
@@ -3,7 +3,6 @@ const RangeMap = require('../shared/occupancy-range-map')
 module.exports = function () {
   return async function setOccupancyRange (env, event) {
     const withinRange = ({ min, max }) => (min <= event.building.occupants && (event.building.occupants <= max || max === null))
-    console.log('event.building.occupants: ' + event.building.occupants + ' \nwithinRange: ' + withinRange + '\nfsec: ' + event.building.fsec)
 
     const fsec = event.building.fsec
     let occupancyRange = event.building.occupants > 0 ? event.building.occupants : ''
