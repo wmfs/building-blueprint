f738be027f2aad9652835e18b22a03d1fcf504e5
wmfsConorF <conor.flanagan@wmfs.net>
2022-05-03 15:46:32 +0100

    fix: add condition to set-occupancy-range to skip if no building record found [sc-14748]

--- functions/set-occupancy-range.js
+++ functions/set-occupancy-range.js
@@ -2,18 +2,20 @@ const RangeMap = require('../shared/occupancy-range-map')
 
 module.exports = function () {
   return async function setOccupancyRange (env, event) {
-    const withinRange = ({ min, max }) => (min <= event.building.occupants && (event.building.occupants <= max || max === null))
+    if (event.building) {
+      const withinRange = ({ min, max }) => (min <= event.building.occupants && (event.building.occupants <= max || max === null))
 
-    const fsec = event.building.fsec
-    let occupancyRange = event.building.occupants > 0 ? event.building.occupants : ''
+      const fsec = event.building.fsec
+      let occupancyRange = event.building.occupants > 0 ? event.building.occupants : ''
 
-    if (RangeMap.smallRangedFsec.includes(fsec)) {
-      occupancyRange = RangeMap.smallRanges.find(withinRange).title
-    } else if (RangeMap.largeRangedFsec.includes(fsec)) {
-      occupancyRange = RangeMap.largeRanges.find(withinRange).title
+      if (RangeMap.smallRangedFsec.includes(fsec)) {
+        occupancyRange = RangeMap.smallRanges.find(withinRange).title
+      } else if (RangeMap.largeRangedFsec.includes(fsec)) {
+        occupancyRange = RangeMap.largeRanges.find(withinRange).title
+      }
+
+      event.building.occupancyRange = occupancyRange
     }
-
-    event.building.occupancyRange = occupancyRange
 
     return event
   }
