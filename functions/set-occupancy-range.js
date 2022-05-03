c10c875a109bc70e015b72e37cd7fa4464592b65
wmfsConorF <conor.flanagan@wmfs.net>
2022-05-03 15:24:43 +0100

    feat: create shared occupancy-range-map with fsecs, ranges for small & large occupancy ranges [sc-14748]

--- functions/set-occupancy-range.js
+++ functions/set-occupancy-range.js
@@ -1,56 +1,17 @@
+const RangeMap = require('../shared/occupancy-range-map')
+
 module.exports = function () {
   return async function setOccupancyRange (env, event) {
-    const smallRangeFsec = ['OTHER_SLEEPING', 'SHOP', 'PUBLIC_BUILDING-OTHER', 'FACTORY', 'OTHER_WORKPLACE']
-    const largeRangeFsec = ['HOSPITAL', 'HOSTEL', 'HOTEL', 'FURTHER_EDUCATION', 'PUBLIC_BUILDING', 'LICENSED_PREMISE', 'SCHOOL', 'OFFICE']
-    const smallRanges = [
-      {
-        title: '1 - 50',
-        min: 1,
-        max: 50
-      },
-      {
-        title: '51 - 200',
-        min: 51,
-        max: 200
-      },
-      {
-        title: '200+',
-        min: 201,
-        max: null
-      }
-    ]
-    const largeRanges = [
-      {
-        title: '1 - 50',
-        min: 1,
-        max: 50
-      },
-      {
-        title: '51 - 200',
-        min: 51,
-        max: 200
-      },
-      {
-        title: '201 - 1000',
-        min: 201,
-        max: 1000
-      },
-      {
-        title: '1000+',
-        min: 1001,
-        max: null
-      }
-    ]
 
     const withinRange = ({ min, max }) => (min <= event.building.occupants && (event.building.occupants <= max || max === null))
 
     const fsec = event.building.fsec
     let occupancyRange = event.building.occupants > 0 ? event.building.occupants : ''
 
-    if (smallRangeFsec.includes(fsec)) {
-      occupancyRange = smallRanges.find(withinRange).title
-    } else if (largeRangeFsec.includes(fsec)) {
-      occupancyRange = largeRanges.find(withinRange).title
+    if (RangeMap.smallRangedFsec.includes(fsec)) {
+      occupancyRange = RangeMap.smallRanges.find(withinRange).title
+    } else if (RangeMap.largeRangedFsec.includes(fsec)) {
+      occupancyRange = RangeMap.largeRanges.find(withinRange).title
     }
 
     event.building.occupancyRange = occupancyRange
---
+++ shared/occupancy-range-map.js
@@ -0,0 +1,60 @@
+const RangeMap = {
+  smallRangedFsec: [
+    'OTHER_SLEEPING',
+    'SHOP',
+    'PUBLIC_BUILDING-OTHER',
+    'FACTORY',
+    'OTHER_WORKPLACE'
+  ],
+  largeRangedFsec: [
+    'HOSPITAL',
+    'HOSTEL',
+    'HOTEL',
+    'FURTHER_EDUCATION',
+    'PUBLIC_BUILDING',
+    'LICENSED_PREMISE',
+    'SCHOOL',
+    'OFFICE'
+  ],
+  smallRanges : [
+    {
+      title: '1 - 50',
+      min: 1,
+      max: 50
+    },
+    {
+      title: '51 - 200',
+      min: 51,
+      max: 200
+    },
+    {
+      title: '200+',
+      min: 201,
+      max: null
+    }
+  ],
+  largeRanges : [
+    {
+      title: '1 - 50',
+      min: 1,
+      max: 50
+    },
+    {
+      title: '51 - 200',
+      min: 51,
+      max: 200
+    },
+    {
+      title: '201 - 1000',
+      min: 201,
+      max: 1000
+    },
+    {
+      title: '1000+',
+      min: 1001,
+      max: null
+    }
+  ]
+}
+
+module.exports = RangeMap
