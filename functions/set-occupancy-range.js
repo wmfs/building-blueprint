a3221052c469f952cbfee4bc76de9ab8f90b4f38
wmfsConorF <conor.flanagan@wmfs.net>
2022-05-04 09:49:31 +0100

    refactor: remove check states and add if-return to function checking for null/undefined occupants [sc-14748]

--- functions/set-occupancy-range.js
+++ functions/set-occupancy-range.js
@@ -2,6 +2,9 @@ const RangeMap = require('../shared/occupancy-range-map')
 
 module.exports = function () {
   return async function setOccupancyRange (env, event) {
+    if (!event.building.occupants) {
+      return event
+    }
     const withinRange = ({ min, max }) => (min <= event.building.occupants && (event.building.occupants <= max || max === null))
 
     const fsec = event.building.fsec
